eb46850fdabddbabb91945e5833ba03d3eed201f
Niels Wadsholt <nwadsholt@ymail.com>
2022-04-17 05:22:49 +0200

    Add method to get time series

--- eloverblik_client.py
+++ eloverblik_client.py
@@ -47,6 +47,34 @@ class MyEnergyClient:
 
         return res.json()
 
+    def get_time_series(self, mentering_point_id: str, start_date: str, end_date: str, aggr='Actual', attempt=1) -> str:
+        access_token = read_from_config('accesstoken')
+
+        res = post(
+            url=f'{self.api_path}/meterdata/gettimeseries/{start_date}/{end_date}/{aggr}',
+            headers={
+                'Authorization': f'Bearer {access_token}'
+            },
+            json={
+                'meteringPoints': {
+                    'meteringPoint': [
+                        mentering_point_id
+                    ]
+                }
+            }
+        )
+
+        if res.status_code == 401 and attempt == 1:
+            print("Unauthorized. Updating access token and retrying ...")
+            self.update_access_token()
+
+            return self.get_metering_point_details(mentering_point_id, attempt=2)
+
+        return res.json()
+
+
+# ===== MANUAL TESTING =====
+
 client = MyEnergyClient()
 
 # ===== TEST: Update access token =====
@@ -55,6 +83,11 @@ client = MyEnergyClient()
 # print(access_token)
 
 # ===== TEST: Get metering point details =====
+# metering_point_id = read_from_config('meteringpointid')
+# metering_point_details = client.get_metering_point_details(metering_point_id)
+# print(metering_point_details)
+
+# ===== TEST: Get time series =====
 metering_point_id = read_from_config('meteringpointid')
-metering_point_details = client.get_metering_point_details(metering_point_id)
-print(metering_point_details)
+time_series = client.get_time_series(metering_point_id, '2022-04-01', '2022-04-02')
+print(time_series)
