9f46aca242cdc18f21b68685a08353f192d68bb0
Niels Wadsholt <nwadsholt@ymail.com>
2022-04-24 03:02:10 +0200

    Add title and query params description to api docs

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from eloverblik_client import EloverblikClient
 from config_handler import read_from_config
 
 __eloverblik_client = EloverblikClient()
 
-app = FastAPI()
+app = FastAPI(title='My Energy App')
 
 origins = [
     'http://localhost:3000'
@@ -19,8 +19,10 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+date_param = Query(None, description= 'Format: \'YYYY-MM-DD\'')
+
 @app.get("/api/timeseries/")
-def get_timeseries(from_date: str, to_date: str):
+def get_timeseries(from_date: str = date_param, to_date: str = date_param):
     metering_point_id = read_from_config('meteringpointid')
     aggr = 'Hour'
 
